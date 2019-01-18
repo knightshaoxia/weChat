54f1876d32fb73e429c713d65bf7ab8d0fb29d99
Xie Liming <xieliming@smartisan.com>
2019-01-18 18:40:12 +0800

    create login activity v00

---
+++ app/src/main/java/android/shaoxia/com/wechat/LoginActivity.java
@@ -0,0 +1,54 @@
+package android.shaoxia.com.wechat;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+
+
+/**
+ * Created by root on 19-1-18.
+ */
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private final static String TAG = "LoginActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_login);
+
+        ImageButton back = (ImageButton) findViewById(R.id.back);
+
+        Log.i(TAG, "ON onCreate");
+        back.setOnClickListener(LoginActivity.this);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "ON stop");
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "ON onPause");
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.back:
+                finish();
+                break;
+            default:
+                break;
+        }
+
+    }
+}
--- app/src/main/java/android/shaoxia/com/wechat/MainActivity.java
+++ app/src/main/java/android/shaoxia/com/wechat/MainActivity.java
@@ -1,8 +1,14 @@
 package android.shaoxia.com.wechat;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +19,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button)findViewById(R.id.login);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "launch login activity", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(MainActivity.this, "click add buttion.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(MainActivity.this, "click remove button", Toast.LENGTH_LONG).show();
+                break;
+            default:
+        }
+
+        return true;
     }
 }
